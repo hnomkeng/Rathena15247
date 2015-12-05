562c4e3ed7d5a8f6f026d9e0df467fb73a56c627
Hnomkeng.B <patcharapon.big@gmail.com>
2015-12-05 12:09:30 +0700

    [ra]แก้ไขสกิล Spider Web
    - ไม่สามารถ Backsliding ได้
    - ไม่สามารถ Bodyrelocation ได้

--- src/map/skill.c
+++ src/map/skill.c
@@ -7583,6 +7583,10 @@ int skill_castend_nodamage_id (struct block_list *src, struct block_list *bl, ui
 		break;
 
 	case TF_BACKSLIDING: //This is the correct implementation as per packet logging information. [Skotlex]
+		if (sd && (sd->sc.data[SC_ANKLE] || sd->sc.data[SC_SPIDERWEB])){  // BY Trying again
+			clif_skill_fail(sd, skill_id, USESKILL_FAIL_LEVEL, 0);
+			return 0;
+		}
 		clif_skill_nodamage(src,bl,skill_id,skill_lv,1);
 		skill_blown(src,bl,skill_get_blewcount(skill_id,skill_lv),unit_getdir(bl),2);
 		break;
@@ -11408,6 +11412,10 @@ int skill_castend_pos2(struct block_list* src, int x, int y, uint16 skill_id, ui
 		return 0; // not to consume item.
 
 	case MO_BODYRELOCATION:
+		if (sd && (sd->sc.data[SC_ANKLE] || sd->sc.data[SC_SPIDERWEB])){  // BY Trying again
+			clif_skill_fail(sd, skill_id, USESKILL_FAIL_LEVEL, 0);
+			return 0;
+		}
 		if (unit_movepos(src, x, y, 1, 1)) {
 #if PACKETVER >= 20111005
 			clif_snap(src, src->x, src->y);
